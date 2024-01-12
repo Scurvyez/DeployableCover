4fd55cca41f44b02fb3e34c2a2108bb34303a6ac
Steve <84108749+Scurvyez@users.noreply.github.com>
2024-01-12 00:16:54 -0500

    -_-

--- 1.4/Source/DeployableCover/ThingClasses/Building_CoverLauncher.cs
+++ 1.4/Source/DeployableCover/ThingClasses/Building_CoverLauncher.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Verse;
+using Verse.AI;
+using Verse.Noise;
 
 namespace DeployableCover
 {
     [StaticConstructorOnStartup]
-    public class Building_CoverLauncher : Building
+    public class Building_CoverLauncher : Building, ITargetingSource
     {
         public static readonly Texture2D LaunchIcon = ContentFinder<Texture2D>.Get("DeployableCover/UI/Commands/LaunchCover");
         private LauncherExtension launcherExtension;
@@ -16,9 +18,25 @@ namespace DeployableCover
         private int lastLaunchTick = -1;
 
         private int TimeSinceLastLaunch => Find.TickManager.TicksGame - lastLaunchTick;
-        private bool CanLaunchNow => compPowerTrader.PowerOn
+
+        /*private bool CanLaunchNow => compPowerTrader.PowerOn
             && TimeSinceLastLaunch > launcherExtension.cooldownTicks
-            && compLauncherStorage.innerContainer.Count > 0;
+            && compLauncherStorage.innerContainer.Count > 0;*/
+
+        public virtual bool MultiSelect => true;
+        public virtual bool CasterIsPawn => false;
+        public virtual Pawn CasterPawn => null;
+        public virtual bool IsMeleeAttack => false;
+        public virtual bool HidePawnTooltips => true;
+        public virtual bool Targetable => true;
+        public virtual Thing Caster => this;
+        public virtual Verb GetVerb => null;
+        public virtual Texture2D UIIcon => null;
+        public virtual TargetingParameters targetParams => new TargetingParameters
+        {
+            canTargetLocations = true
+        };
+        public virtual ITargetingSource DestinationSelector => null;
 
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
@@ -40,11 +58,12 @@ namespace DeployableCover
                 defaultLabel = "Deploy cover",
                 defaultDesc = "Designate an area for launching deployable cover.",
                 icon = LaunchIcon,
-                action = LaunchCoverArea,
-                disabled = !CanLaunchNow
+                action = ,
+                disabled = !ValidateTarget() && !CanHitTarget()
             };
         }
 
+        /*
         private void LaunchCoverArea()
         {
             Map map = Map;
@@ -67,15 +86,51 @@ namespace DeployableCover
                 lastLaunchTick = Find.TickManager.TicksGame;
             });
         }
-        
+        */
+
+        public virtual void OrderForceTarget(LocalTargetInfo target)
+        {
+            
+        }
+
+        public virtual bool CanHitTarget(LocalTargetInfo target)
+        {
+            if (Caster == null || !Caster.Spawned)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public virtual bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            if (!target.Cell.InBounds(Map) && !target.Cell.Standable(Map) && target.Cell.GetFirstBuilding(Map) != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public virtual void DrawHighlight(LocalTargetInfo target)
+        {
+            if (target.IsValid)
+            {
+                GenDraw.DrawTargetHighlight(target);
+            }
+        }
+
+        public virtual void OnGUI(LocalTargetInfo target) /* FIX THIS LATER */
+        {
+            Texture2D icon = ((!target.IsValid) ? TexCommand.CannotShoot : ((!(UIIcon != BaseContent.BadTex)) ? TexCommand.Attack : UIIcon));
+            GenUI.DrawMouseAttachment(icon);
+        }
+
         public void MakeFlyer(IntVec3 target, Map map)
         {
             Thing ammo = compLauncherStorage.GetDirectlyHeldThings().RandomElement();
-
             if (ammo != null)
             {
                 CoverFlyer flyer = ThingMaker.MakeThing(CoreDefOf.SW_CoverFlyer, ammo.Stuff) as CoverFlyer;
-
                 if (flyer != null)
                 {
                     flyer.CoverStartCell = this.Position;
