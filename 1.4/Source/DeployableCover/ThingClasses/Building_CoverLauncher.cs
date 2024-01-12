f1155d89face45fb241a2813609d1fffff4b93b9
Steve <84108749+Scurvyez@users.noreply.github.com>
2024-01-12 16:32:48 -0500

    We're all good

--- 1.4/Source/DeployableCover/ThingClasses/Building_CoverLauncher.cs
+++ 1.4/Source/DeployableCover/ThingClasses/Building_CoverLauncher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Verse;
+using Verse.Noise;
 
 namespace DeployableCover
 {
@@ -14,6 +15,7 @@ namespace DeployableCover
         private CompPowerTrader compPowerTrader;
         private CompLauncherStorage compLauncherStorage;
         private int lastLaunchTick = -1;
+        private int launchAmount = 1;
 
         private int TimeSinceLastLaunch => Find.TickManager.TicksGame - lastLaunchTick;
 
@@ -38,15 +40,36 @@ namespace DeployableCover
 
             yield return new Command_Action
             {
+                defaultLabel = "Set launch amount",
+                defaultDesc = "Designate how many cover pieces to launch.",
+                icon = LaunchIcon,
+                action = SetLaunchAmount,
+                disabled = false,
+            };
+
+            yield return new Command_Action
+            {
                 defaultLabel = "Deploy cover",
                 defaultDesc = "Designate an area for launching deployable cover.",
                 icon = LaunchIcon,
-                action = LaunchCoverArea,
+                action = LaunchCover,
                 disabled = !CanLaunchNow
             };
         }
 
-        private void LaunchCoverArea()
+        private void SetLaunchAmount()
+        {
+            int amountAvail = compLauncherStorage.innerContainer.Count;
+            if (amountAvail > 0)
+            {
+                Find.WindowStack.Add(new Dialog_Slider("Select Launch Amount", 1, amountAvail, value =>
+                {
+                    launchAmount = Mathf.RoundToInt(value);
+                }));
+            }
+        }
+
+        private void LaunchCover()
         {
             Map map = Map;
             IntVec3 position = Position;
@@ -60,35 +83,49 @@ namespace DeployableCover
                     targetCell.Cell.InBounds(map) &&
                     targetCell.Cell.Standable(map) &&
                     targetCell.Cell.GetFirstBuilding(map) == null &&
-                    targetCells.Contains(targetCell.Cell),
+                    !targetCell.Cell.Fogged(map)
             }, delegate (LocalTargetInfo target)
             {
-                MakeFlyer(target.Cell, map);
+                MakeFlyers(target.Cell, map);
                 lastLaunchTick = Find.TickManager.TicksGame;
             });
         }
 
-        public void MakeFlyer(IntVec3 target, Map map)
+        public void MakeFlyers(IntVec3 target, Map map)
         {
-            Thing ammo = compLauncherStorage.GetDirectlyHeldThings().RandomElement();
-            if (ammo != null)
-            {
-                CoverFlyer flyer = ThingMaker.MakeThing(CoreDefOf.SW_CoverFlyer, ammo.Stuff) as CoverFlyer;
-                if (flyer != null)
-                {
-                    flyer.CoverStartCell = this.Position;
-                    flyer.CoverDestCell = target;
+            List<Thing> ammoList = compLauncherStorage.GetDirectlyHeldThings().ToList();
+            int ammoToLaunch = Mathf.Min(launchAmount, ammoList.Count);
 
-                    GenSpawn.Spawn(flyer, flyer.CoverDestCell, map);
+            for (int i = 0;  i < ammoToLaunch; i++)
+            {
+                Thing ammo = compLauncherStorage.GetDirectlyHeldThings().RandomElement();
+                if (ammo != null)
+                {
+                    CoverFlyer flyer = ThingMaker.MakeThing(CoreDefOf.SW_CoverFlyer, ammo.Stuff) as CoverFlyer;
+                    if (flyer != null)
+                    {
+                        flyer.CoverStartCell = this.Position;
+                        flyer.CoverDestCell = GetRandomCellInRadius(target, 5, map);
+
+                        GenSpawn.Spawn(flyer, flyer.CoverDestCell, map);
+                    }
+                    compLauncherStorage.innerContainer.Remove(ammo);
                 }
-                compLauncherStorage.innerContainer.Remove(ammo);
             }
+        }
+
+        private IntVec3 GetRandomCellInRadius(IntVec3 center, int radius, Map map)
+        {
+            IEnumerable<IntVec3> cellsInRadius = GenRadial.RadialCellsAround(center, radius, true).Where(cell =>
+                cell.IsValid && !cell.Fogged(map) && cell.Walkable(map) && cell.GetFirstBuilding(map) == null);
+            return cellsInRadius.RandomElement();
         }
 
         public override void ExposeData()
         {
             base.ExposeData();
             Scribe_Values.Look(ref lastLaunchTick, "lastLaunchTick", -1);
+            Scribe_Values.Look(ref launchAmount, "launchAmount", 1);
         }
     }
 }
